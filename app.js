0a970b93e5a8890d48679db3a58960371879226b
Vinothkumarsece <105229310+Vinothkumarsece@users.noreply.github.com>
2023-07-07 04:07:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Animation for Full-Page Contact Form
+window.addEventListener('DOMContentLoaded', function() {
+    var contactForm = document.getElementById('contact-form');
+    contactForm.style.animation = 'slideInFromLeft 1s ease-in';
+    contactForm.style.opacity = 1;
+});
